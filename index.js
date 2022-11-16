0a1f4f8e448d393b95d41e46d6f204cc2b40797d
mindkeeper <fcb.nyak@gmail.com>
2022-11-16 16:28:11 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const morgan = require("morgan");
+require("dotenv").config();
+
+const { PORT } = process.env;
+// const db = require("./src/config/postgre");
+const mainRouter = require("./src/routes/main");
+const server = express();
+const cors = require("cors");
+// require("./src/config/redis");
+
+server.use(cors());
+server.use(express.json());
+// server.use(express.static("./public/images"));
+//parser encoded
+server.use(express.urlencoded({ extended: false }));
+server.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms")
+);
+server.use(mainRouter);
+server.listen(PORT, () => {
+  console.log(`Server Running at Port ${PORT}`);
+});
---
+++ src/routes/main.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const mainRouter = express.Router();
+
+mainRouter.get(`/`, (req, res) => {
+  res.json({ msg: "Welcome" });
+});
+
+module.exports = mainRouter;
