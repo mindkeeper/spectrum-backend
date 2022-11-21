5828f6770ddd4832098ccdd9c02f01abd031ee71
mindkeeper <fcb.nyak@gmail.com>
2022-11-21 19:17:11 +0700

    fix categories when stock 0

--- src/repo/categories.js
+++ src/repo/categories.js
@@ -3,7 +3,7 @@ const db = require("../config/postgre");
 const getCategory = () => {
   return new Promise((resolve, reject) => {
     const query =
-      "select c.id , c.category_name ,count(pc.product_id) as total_product  from categories c join product_categories pc on pc.category_id = c.id join products p on p.id = pc.product_id where p.deleted_at is null group by c.id;";
+      "select c.id , c.category_name ,count(pc.product_id) as total_product  from categories c join product_categories pc on pc.category_id = c.id join products p on p.id = pc.product_id where p.deleted_at is null and p.stock != 0 group by c.id;";
 
     db.query(query, (error, result) => {
       if (error) {
